6f5b3969db78d96e0d1048f57e8a1b2eb88b4e55
Beardocracy <travisjbearden@gmail.com>
2020-05-22 18:06:32 -0700

    Initial commit

---
+++ content.js
@@ -0,0 +1,7 @@
+alert("You are in the good place");
+
+const button = document.querySelector('#task-test-correction-914-correction-modal > div > div > div.modal-body > div.actions > center > input');
+
+button.addEventListener('click', function () {
+  alert("It really took you all day to get to this point?");
+}, false);
