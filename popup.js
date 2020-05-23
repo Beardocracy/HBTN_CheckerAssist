60771d7c757a0fc39665c9f65970738536879fb0
Beardocracy <travisjbearden@gmail.com>
2020-05-22 20:57:52 -0700

    Added dynamic title in popup

--- popup.js
+++ popup.js
@@ -1,1 +1,5 @@
-document.querySelector('');
+// Sets the title to the project's title, using the storage api. Value was set
+// in the content.js file.
+chrome.storage.sync.get(['title'], (result) => {
+  document.querySelector('#project_title').innerHTML = result.title;
+});
