e3c1644305274ec977093576396ca2d4975fa39c
Beardocracy <travisjbearden@gmail.com>
2020-05-22 18:46:43 -0700

    Added popup

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-alert("You are in the good place");
-
 const button = document.querySelector('#task-test-correction-914-correction-modal > div > div > div.modal-body > div.actions > center > input');
 
 button.addEventListener('click', function () {
